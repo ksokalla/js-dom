14577cd29f1d26eb740bde3bd6a8033ae93f7fdf
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 11:46:14 +0100

    nodes init

--- js/1-dom.js
+++ js/1-dom.js
@@ -1,3 +1,10 @@
 'use strict';
 
+console.log(window);
+console.log(document.innerHeight);
+console.log(document.outerHeight);
+
+console.log(document);
+console.log(document.documentElement);
+console.log(document.head);
 console.log(document.body);
--- js/2-search-element.js
+++ js/2-search-element.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var linki = document.getElementsByClassName('link');
+console.log(linki);
+
+var tagi = document.getElementsByTagName('a');
+console.log(tagi);
+
+var linkiPoSelektorze = document.querySelectorAll('.link');
+console.log(linkiPoSelektorze);
+
