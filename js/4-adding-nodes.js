e3f31f3036aaa8095a2856d1b687e29017426cd3
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 14:10:39 +0100

    add html init

--- js/4-adding-nodes.js
+++ js/4-adding-nodes.js
@@ -1,2 +1,28 @@
 'use strict';
+//'use strict';
+//
+//var istniejacyWezel = document.getElementById('parFirst').children[2];
+//
+//console.log(istniejacyWezel);
+//
+//var newElement = document.createElement('p');
+//
+//var newElementContent = document.createTextNode('To jest nowy paragraf');
+//
+//newElement.appendChild(newElementContent);
+//
+//console.log(newElement);
+//
+//document.getElementById('parFirst').replaceChild(newElement, istniejacyWezel);
+
+var usuwanieBR = document.getElementsByTagName('a');
+
+console.log(usuwanieBR);
+
+for (var i=0; i<usuwanieBR.length; i++ ) {
+	console.log(usuwanieBR[i]);
+	var br = document.createElement('br');
+	usuwanieBR[i].parentNode.insertBefore(br, usuwanieBR[i].nextSibling);
+	usuwanieBR[i].removeAttribute('class');
+}
 
