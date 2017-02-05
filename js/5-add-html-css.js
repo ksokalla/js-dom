1eadc04fb299d180a479d860da8fe5ed9fbe7338
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 14:27:21 +0100

    events init

--- js/5-add-html-css.js
+++ js/5-add-html-css.js
@@ -1,2 +1,13 @@
 'use strict';
 
+var mainHeader = document.getElementById('main-header');
+
+mainHeader.innerHTML = "Treść nagłówka";
+
+var link = document.getElementsByClassName('link')[0];
+
+link.ref = "http://akademia108.pl"; 
+
+link.className = "nowa-klasa";
+
+mainHeader.style.color = "#11aa22";
